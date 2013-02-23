0cf5e7784b04118854c37088a6a480cb1d3afa37
Ryan L. Cross <rcross@gmail.com>
2013-02-23 15:04:26 -0800

    Update alises and config vars.

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,10 +1,19 @@
 # Heroku
+
+alias hca="hs sharing:add $HEROKU_EMAIL -a"
+alias hcr="hs sharing:remove $HEROKU_EMAIL -a"
 alias hs="heroku sudo"
-alias hsa="heroku sudo apps -u $1"
+alias hsa="heroku sudo addons -a $1"
 alias hsc="heroku sudo certs -a $1"
+alias hsconf="heroku sudo config -a $1"
 alias hsd="heroku sudo domains -a $1"
 alias hsi="heroku sudo info -a $1"
-alias hsconf="heroku sudo config -a $1"
+alias hsix="heroku sudo info -x -a $1"
+alias hsl="heroku sudo logs -t -a $1"
+alias hslabs="heroku sudo labs -a $1"
+alias hsp="heroku sudo ps -a $1"
+alias hsu="heroku sudo apps -u $1"
+alias ic="ion-client"
 
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
